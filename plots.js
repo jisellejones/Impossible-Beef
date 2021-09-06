5f112480fe4b775f8504a0e3adff1caabaf7cb66
Adenine <30560532+jisellejones@users.noreply.github.com>
2021-09-06 13:51:02 -0700

    rearranged files for viewing on git pages

--- plots.js
+++ plots.js
@@ -1,31 +1,37 @@
-function init() {
-    data = [{
-      x: [1, 2, 3, 4, 5],
-      y: [1, 2, 4, 8, 16] }];
-    Plotly.newPlot("plot", data);
-  };
-  
-  d3.selectAll("#dropdownMenu").on("change", updatePlotly);
-  function updatePlotly() {
-    var dropdownMenu = d3.select("#dropdownMenu");
-    var dataset = dropdownMenu.property("value");
-  
-    var xData = [1, 2, 3, 4, 5];
-    var yData = [];
-  
-    if (dataset === 'dataset1') {
-      yData = [1, 2, 4, 8, 16];
-    };
-  
-    if (dataset === 'dataset2') {
-      yData = [1, 10, 100, 1000, 10000];
-    };
-  
-    var trace = {
-      x: [xData],
-      y: [yData],
-    };
-    Plotly.restyle("plot", trace);
-  };
-  
-  init();
+// Sort the data array using the greekSearchResults value
+data.sort(function(a, b) {
+  return parseFloat(b.greekSearchResults) - parseFloat(a.greekSearchResults);
+});
+
+// Slice the first 10 objects for plotting
+data = data.slice(0, 10);
+
+// Reverse the array due to Plotly's defaults
+data = data.reverse();
+
+// Trace1 for the Greek Data
+var trace1 = {
+  x: data.map(row => row.greekSearchResults),
+  y: data.map(row => row.greekName),
+  text: data.map(row => row.greekName),
+  name: "Greek",
+  type: "bar",
+  orientation: "h"
+};
+
+// data
+var data = [trace1];
+
+// Apply the group bar mode to the layout
+var layout = {
+  title: "Greek gods search results",
+  margin: {
+    l: 100,
+    r: 100,
+    t: 100,
+    b: 100
+  }
+};
+
+// Render the plot to the div tag with id "plot"
+Plotly.newPlot("plot", data, layout);
